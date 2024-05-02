b7526c63012df0f5c204b4c2dd700d8d6dbf399a
mohammad-tarah <aqatarahomi8@gmail.com>
2024-05-02 19:31:03 +0330

    ok

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,10 +1,18 @@
 AOS.init();
-function change() {
-    document.body.classList.toggle("dark-theme");
-    if (document.documentElement.getAttribute('data-bs-theme') == 'dark') {
-        document.documentElement.setAttribute('data-bs-theme','light')
-    }
-    else {
-        document.documentElement.setAttribute('data-bs-theme','dark')
-    }
-}
+const dark_mode = document.getElementById("dark-mode")
+dark_mode.addEventListener("click", () => {
+
+   const dark_card = document.getElementsByClassName("card")
+   document.body.classList.toggle("dark-theme")
+   for (let i = 0; i < dark_card.length; i++) {
+      dark_card[i].classList.toggle("card-dark")
+   }
+
+
+   if (document.documentElement.getAttribute('data-bs-theme') == 'dark') {
+      document.documentElement.setAttribute('data-bs-theme', 'light')
+   } else {
+      document.documentElement.setAttribute('data-bs-theme', 'dark')
+   }
+})
+
